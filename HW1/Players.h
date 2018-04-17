910c29a6ffa6a1b10b1a758b5a78e3cf831b3dd1
nemonom <nemonom230@gmail.com>
2018-04-17 21:35:08 +0900

    replay

--- HW1/Players.h
+++ HW1/Players.h
@@ -30,8 +30,15 @@ public:
 	int GetScore1() const { return score1; }
 	int GetScore2() const { return score2; }
 
+	void SetScore1(int n) { score1 = n; }
+	void SetScore2(int n) { score2 = n; }
+
+
 	void SetRank1(int n) { rank1 = n; }
 	void SetRank2(int n) { rank2 = n; }
+
+	void Save_Rank1() { past_rank1 = rank1; }
+	void Save_Rank2() { past_rank2 = rank2; }
 
 };
 
