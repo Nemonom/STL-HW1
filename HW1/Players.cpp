c60c94584724c6a0f592280c47eedece8945af25
nemonom <nemonom230@gmail.com>
2018-04-18 02:52:32 +0900

    select, play, result

--- HW1/Players.cpp
+++ HW1/Players.cpp
@@ -13,24 +13,28 @@ Players::~Players()
 
 void Players::Show1() const
 {
-	std::cout << id << ", ÄíÅ°ÈÆ·Ã¼Ò : " << score1 << "(" << rank1 << ")µî" << std::endl;
+	std::cout << id << ", ÄíÅ°ÈÆ·Ã¼Ò : " << score1 << "Á¡ (" << rank1 << ")µî" << std::endl;
 }
 void Players::Show2() const
 {
-	std::cout << id << ", ¶¼Å»Ãâ : " << score2 << "(" << rank2 << ")µî" << std::endl;
+	std::cout << id << ", ¶¼Å»Ãâ : " << score2 << "Á¡ (" << rank2 << ")µî" << std::endl;
 }
 
 void Players::Show() const
 {
-	std::cout << id << ", ÄíÅ°ÈÆ·Ã¼Ò : " << score1 << "(" << rank1 << ")µî, ¶¼Å»Ãâ : " 
-									<< score2 << "(" << rank2 << ")µî" << std::endl;
+	std::cout << id << ", ÄíÅ°ÈÆ·Ã¼Ò : " << score1 << "Á¡ (" << rank1 << ")µî, ¶¼Å»Ãâ : " 
+									<< score2 << "Á¡ (" << rank2 << ")µî" << std::endl;
 }
 
 void Players::My_Show() const
 {
-	std::cout << id << ", ÄíÅ°ÈÆ·Ã¼Ò : " << score1 << "(" << rank1 << ")µî" << std::endl;
-	std::cout << "ÄíÅ°ÈÆ·Ã¼Ò ·©Å· º¯È­ : " << rank1 - past_rank1 << std::endl;
+	std::cout << "=================================" << std::endl;
+	std::cout << "ÄíÅ°ÈÆ·Ã¼Ò : " << score1 << "Á¡ (" << rank1 << ")µî"
+		<< ", ÄíÅ°ÈÆ·Ã¼Ò ·©Å· º¯È­ : " << rank1 - past_rank1
+		<< ", »óÀ§ " << rank1 << "%" << std::endl;
 	
-	std::cout << id << ", ¶¼Å»Ãâ : " << score2 << "(" << rank2 << ")µî" << std::endl;
-	std::cout << "‹óÅ»Ãâ ·©Å· º¯È­ : " << rank2 - past_rank2 << std::endl;
+	std::cout << "¶¼Å»Ãâ : " << score2 << "Á¡ (" << rank2 << ")µî"
+		<< ", ¶¼Å»Ãâ ·©Å· º¯È­ : " << rank2 - past_rank2 
+		<< ", »óÀ§ " << rank2 << "%" << std::endl;	
+	std::cout << "=================================" << std::endl;
 }
